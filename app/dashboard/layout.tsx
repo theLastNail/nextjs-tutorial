30c1f8d190fc27477fc00720cf6011626690e20b
Tomas <puk@centrum.cz>
2024-02-15 16:10:56 +0100

    next commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default ({ children }: { children: React.ReactNode }) => {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
@@ -9,4 +9,4 @@ export default ({ children }: { children: React.ReactNode }) => {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-};
+}
